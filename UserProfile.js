324f06de2b013e3e86cc6bf323b6beca123f873e
Jamela <Jamelagalinato8@gmail.com>
2024-10-31 19:14:38 +0800

    UUserProfile.js

--- UserProfile.js
+++ UserProfile.js
@@ -1,19 +1,17 @@
 // UserProfile.js
 
 // Collect user information
-let firstName = prompt("Jamela:");
-let lastName = prompt("Galinato:");
-let age = prompt("19:");
-let favoriteNumber = prompt("11:");
-let favoriteColor = prompt("Purple:");
+let firstName = prompt("Jamela: ");
+let lastName = prompt("Galinato: ");
+let age = prompt("19: ");
+let favoriteNumber = prompt("11: ");
+let favoriteColor = prompt("Purple: ");
 
-// Store the information in variables
-let userProfile = {
-  fullName: firstName + " " + lastName,
-  age: age,
-  favoriteNumber: favoriteNumber,
-  favoriteColor: favoriteColor,
-};
+// Calculate full name
+let fullName = firstName + " " + lastName;
 
-// Log the user profile to the console
-console.log(userProfile);
+// Logging the information to the console
+console.log("fullname:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
